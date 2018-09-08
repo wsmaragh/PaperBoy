8dc3de3033b28dbc946f5276c71832cd2f39c09f
Winston Maragh <wsmaragh@gmail.com>
2018-09-08 11:33:58 -0400

    Initial Commit

---
+++ PaperBoyTests/PaperBoyTests.swift
@@ -0,0 +1,36 @@
+//
+//  PaperBoyTests.swift
+//  PaperBoyTests
+//
+//  Created by Winston Maragh on 9/8/18.
+//  Copyright © 2018 Winston Maragh. All rights reserved.
+//
+
+import XCTest
+@testable import PaperBoy
+
+class PaperBoyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
